55cca4c2cf06d38d10e65e81b68dcd93b1b0f70f
mshindzhev <shindzhev@gmail.com>
2025-09-08 01:12:35 +0300

    Обновил conftest для работы c Selenoid

--- conftest.py
+++ conftest.py
@@ -1,4 +1,4 @@
-import os
+from selenium.webdriver.chrome.options import Options
 
 import pytest
 from selenium import webdriver
@@ -8,19 +8,13 @@ from page_object.pages.authorization_page import AuthorizationPage
 
 @pytest.fixture()
 def driver():
-    options = webdriver.ChromeOptions()
+    options = Options()
     options.add_argument('--window-size=1920,1080')
 
-    # Проверяем, запущены ли мы в контейнере с Selenoid
-    if os.environ.get('SELENOID_ENABLED', 'false') == 'true':
-        # Используем Remote WebDriver для Selenoid
-        driver = webdriver.Remote(
-            command_executor='http://selenoid:4444/wd/hub',
-            options=options
-        )
-    else:
-        # Локальный ChromeDriver (для разработки)
-        driver = webdriver.Chrome(options=options)
+    driver = webdriver.Remote(
+        command_executor='http://selenoid:4444/wd/hub',
+        options=options
+    )
 
     driver.implicitly_wait(10)
     yield driver
