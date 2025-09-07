b39e2275c826112c5ac8044b0f63564ae318ddd3
mshindzhev <shindzhev@gmail.com>
2025-09-08 01:02:07 +0300

    Обновил docker-compose.yml

--- conftest.py
+++ conftest.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 from selenium import webdriver
 
@@ -8,7 +10,19 @@ from page_object.pages.authorization_page import AuthorizationPage
 def driver():
     options = webdriver.ChromeOptions()
     options.add_argument('--window-size=1920,1080')
-    driver = webdriver.Chrome(options=options)
+
+    # Проверяем, запущены ли мы в контейнере с Selenoid
+    if os.environ.get('SELENOID_ENABLED', 'false') == 'true':
+        # Используем Remote WebDriver для Selenoid
+        driver = webdriver.Remote(
+            command_executor='http://selenoid:4444/wd/hub',
+            options=options
+        )
+    else:
+        # Локальный ChromeDriver (для разработки)
+        driver = webdriver.Chrome(options=options)
+
+    driver.implicitly_wait(10)
     yield driver
     driver.quit()
 
